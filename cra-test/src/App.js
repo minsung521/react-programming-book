b49125d73d814786158432d8f68e09a53c326968
minsung521 <mingsung521@gmail.com>
2021-11-30 11:37:50 +0900

    28p

--- cra-test/src/App.js
+++ cra-test/src/App.js
@@ -2,12 +2,14 @@ import logo from "./logo.svg";
 import "./App.css";
 import big from "./big.jpg";
 import small from "./small.jpg";
+import TodoList from "./TodoList";
 
 function App() {
 	return (
 		<div className="App">
-			<img src={big} alt="big"></img>
-			<img src={small} alt="small"></img>
+			{/* <img src={big} alt="big"></img>
+			<img src={small} alt="small"></img> */}
+			<TodoList />
 		</div>
 	);
 }
